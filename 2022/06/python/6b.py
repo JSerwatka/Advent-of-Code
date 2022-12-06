fb7a89d3b30f646ae81bd63ca7f24451234198ea
Jakub Serwatka <kubaserw@gmail.com>
2022-12-06 13:18:45 +0100

    add 6a and 6b (python)

---
+++ 2022/06/python/6b.py
@@ -0,0 +1,17 @@
+def find_first_marker(code, marker_size = 4):
+    try:
+        for index in range(len(code) - marker_size):
+            potencial_marker = code[index : (index + marker_size)]
+            if (len(potencial_marker) == len(set(potencial_marker))):
+                return index + marker_size
+    except IndexError:
+        print("Out of range - check your code")
+
+def main(input_file):
+    with open(input_file) as f:
+        stream = f.read() 
+        marker_index = find_first_marker(stream, 14)
+    return marker_index
+        
+        
+print(main("../input.txt"))
