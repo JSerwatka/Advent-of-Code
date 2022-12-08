bdbb59ebb6e4e9a7cf22c5c5df608d836c3b5eec
Jakub Serwatka <kubaserw@gmail.com>
2022-12-08 19:20:10 +0100

    add 8b (python)

--- 2022/08/python/8b.py
+++ 2022/08/python/8b.py
@@ -1,25 +1,28 @@
+from functools import reduce
+
 def create_array_from_string(line):
     return [int(tree) for tree in line]
 
 
-def sort_short_to_long_path(tree_ccords, array_length):
+def get_paths(tree_ccords, array_length):
     row, column = tree_ccords
 
-    paths = {
+    return {
         "left": column,
         "top": row,
         "right": array_length - column - 1,
         "bottom": array_length - row - 1
     }
-    return {k: v for k, v in sorted(paths.items(), key=lambda item: item[1])}
 
 
-def check_if_tree_visible(tree_coords, paths_sorted, arr):
+def get_scenic_score(tree_coords, paths, arr):
     tree_value = arr[tree_coords[0]][tree_coords[1]]
+    scenic_scores = []
 
-    for direction, steps in paths_sorted.items():
+    for direction, steps in paths.items():
+        scenic_score_for_direction = 0
         current_tree = tree_coords.copy()
-        for move in range(1, steps + 1):
+        for _ in range(1, steps + 1):
             match direction:
                 case "left":
                     current_tree[1] = current_tree[1] - 1
@@ -29,38 +32,37 @@ def check_if_tree_visible(tree_coords, paths_sorted, arr):
                     current_tree[1] = current_tree[1] + 1
                 case "bottom":
                     current_tree[0] = current_tree[0] + 1
+            
+            scenic_score_for_direction += 1
             # Found tree larger then starting tree
             if arr[current_tree[0]][current_tree[1]] >= tree_value:
                 break
-            # Moved to the end - all trees smaller
-            if move == steps:
-                return True
-    return False
 
-            
+        scenic_scores.append(scenic_score_for_direction)
 
+    return reduce((lambda x, y: x * y), scenic_scores)
 
 def main(input_file):
     arr = []
-
+    highest_scenic_score = 0
     with open(input_file) as f:
         for line in f:
             line = line.strip()
             arr.append(create_array_from_string(line))
 
     array_length = len(arr)
-    # Calculate edge trees
-    visible_trees = array_length * 4 - 4
 
     # Go thorugh all non-edge trees
     for row in range(1, array_length - 1):
         for column in range(1, array_length - 1):
             tree_coords = [row, column]
 
-            paths_sorted = sort_short_to_long_path(tree_coords, array_length)
-            if check_if_tree_visible(tree_coords, paths_sorted, arr):
-                visible_trees += 1
-    return visible_trees
+            paths = get_paths(tree_coords, array_length)
+            scenic_score = get_scenic_score(tree_coords, paths, arr)
+            
+            if scenic_score > highest_scenic_score:
+                highest_scenic_score = scenic_score
+    return highest_scenic_score
 
 
 print(main("../input.txt"))
