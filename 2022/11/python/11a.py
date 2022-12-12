44bd9233ed05b194a8e87b8bbd4863ffcf46891d
Jakub Serwatka <kubaserw@gmail.com>
2022-12-12 11:37:39 +0100

    add 11a (python)

---
+++ 2022/11/python/11a.py
@@ -0,0 +1,61 @@
+from input import *
+
+class Monkey:
+    def __init__(self, hold_items, operation, test_number, true_destination, false_destination):
+        self.hold_items = hold_items
+        self.amount_of_inspections = 0
+        self.test_number = test_number
+        self.true_destination = true_destination
+        self.false_destination = false_destination
+        self.operation = operation
+        
+    def run_turn(self):
+        items_by_monkey = []
+        for item in self.hold_items:
+            item = self.operation(item)
+            item //= 3
+            if item % self.test_number == 0:
+                items_by_monkey.append((self.true_destination, item))
+            else:
+                items_by_monkey.append((self.false_destination, item))
+            self.amount_of_inspections += 1
+
+        self.hold_items = []
+        return items_by_monkey
+
+    def __str__(self):
+        items_as_string = " ".join(map(str, self.hold_items))
+        tab = 4 * " "
+        ouput_string = "Monkey:\n"
+        ouput_string += f"{tab}Items: {items_as_string}\n"
+        ouput_string += f"{tab}Amount of inspections: {self.amount_of_inspections}\n"
+        
+        return ouput_string
+    
+    def __repr__(self):
+        return self.__str__()
+
+
+def caluclate_monkey_bussiness(monkeys):
+    inspection_amounts = []
+    for monkey in monkeys:
+        inspection_amounts.append(monkey.amount_of_inspections)
+        
+    inspection_amounts.sort(reverse=True)
+    return inspection_amounts[0] * inspection_amounts[1]
+        
+def main(monkeys_logic, amount_of_rounds):
+    monkeys_dict = {}
+    for monkey_number, monkey_logic in enumerate(monkeys_logic):
+        monkeys_dict[monkey_number] = Monkey(monkey_logic["starting_items"], monkey_logic["operation"], monkey_logic["test_number"], monkey_logic["true_destination"], monkey_logic["false_destination"])
+        
+    for _ in range(amount_of_rounds):
+        for current_monkey in monkeys_dict.values():
+            items_to_throw = current_monkey.run_turn()
+            
+            for item_to_throw in items_to_throw:
+                monkeys_dict[item_to_throw[0]].hold_items.append(item_to_throw[1])
+
+    print(caluclate_monkey_bussiness(monkeys_dict.values()))
+
+main(monkeys, 20)
---
+++ 2022/11/python/input.py
@@ -0,0 +1,89 @@
+monkeys_test = [
+    {
+        "starting_items": [79, 98],
+        "operation": lambda x: x * 19,
+        "test_number": 23,
+        "true_destination": 2,
+        "false_destination": 3
+    },
+    {
+        "starting_items": [54, 65, 75, 74],
+        "operation": lambda x: x + 6,
+        "test_number": 19,
+        "true_destination": 2,
+        "false_destination": 0
+    },
+    {
+        "starting_items": [79, 60, 97],
+        "operation": lambda x: x * x,
+        "test_number": 13,
+        "true_destination": 1,
+        "false_destination": 3
+    },
+    {
+        "starting_items": [74],
+        "operation": lambda x: x + 3,
+        "test_number": 17,
+        "true_destination": 0,
+        "false_destination": 1
+    }
+]
+
+monkeys = [
+    {
+        "starting_items": [64, 89, 65, 95],
+        "operation": lambda x: x * 7,
+        "test_number": 3,
+        "true_destination": 4,
+        "false_destination": 1
+    },
+    {
+        "starting_items": [76, 66, 74, 87, 70, 56, 51, 66],
+        "operation": lambda x: x + 5,
+        "test_number": 13,
+        "true_destination": 7,
+        "false_destination": 3
+    },
+    {
+        "starting_items": [91, 60, 63],
+        "operation": lambda x: x * x,
+        "test_number": 2,
+        "true_destination": 6,
+        "false_destination": 5
+    },
+    {
+        "starting_items": [92, 61, 79, 97, 79],
+        "operation": lambda x: x + 6,
+        "test_number": 11,
+        "true_destination": 2,
+        "false_destination": 6
+    },
+    {
+        "starting_items": [93, 54],
+        "operation": lambda x: x * 11,
+        "test_number": 5,
+        "true_destination": 1,
+        "false_destination": 7
+    },
+    {
+        "starting_items": [60, 79, 92, 69, 88, 82, 70],
+        "operation": lambda x: x + 8,
+        "test_number": 17,
+        "true_destination": 4,
+        "false_destination": 0
+    },
+    {
+        "starting_items": [64, 57, 73, 89, 55, 53],
+        "operation": lambda x: x + 1,
+        "test_number": 19,
+        "true_destination": 0,
+        "false_destination": 5
+    },
+    {
+        "starting_items": [62],
+        "operation": lambda x: x + 4,
+        "test_number": 7,
+        "true_destination": 3,
+        "false_destination": 2
+    }
+]
