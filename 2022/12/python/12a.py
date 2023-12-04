2a4e9c27193357fa5b81f2519d95cbd55615b832
Jakub Serwatka <33938646+JSerwatka@users.noreply.github.com>
2023-12-04 12:02:08 +0100

    add 11,12,13 (2022)

---
+++ 2022/12/python/12a.py
@@ -0,0 +1,6 @@
+def main(input_file):
+    with open(input_file) as f:
+        for command in f:
+            pass
+
+print(main("../input_test.txt"))
---
+++ 2022/13/python/13a.py
@@ -0,0 +1,11 @@
+def check_if_array_in_right_order(arr1, arr2):
+    for 
+
+def main(input_file):
+    with open(input_file) as f:
+        for command in f:
+            pass
+
+print(main("../input_test.txt"))
+
+# https://note.nkmk.me/en/python-numpy-ndarray-ndim-shape-size/
