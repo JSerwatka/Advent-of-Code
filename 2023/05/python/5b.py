32068034a378db354a665e6b6d8dc0e9f3803774
Jakub Serwatka <33938646+JSerwatka@users.noreply.github.com>
2023-12-05 19:12:39 +0100

    feat: add range checking

--- 2023/05/python/5b.py
+++ 2023/05/python/5b.py
@@ -8,27 +8,70 @@ def get_seeds(seed_line: str):
     seed_numbers = string_array_to_array_of_int(seed_numbers_str_arr)
     seed_numbers_grouped = list(zip(*[iter(seed_numbers)] * 2))
     seed_ranges = [range(range_start, range_start + range_len) for range_start, range_len in seed_numbers_grouped]
-    
-    return [x for seed_range in seed_ranges for x in seed_range]
+    print(seed_ranges)
+    # return [x for seed_range in seed_ranges for x in seed_range]    
 
-def map_seeds_to_new_values(map_values, last_seeds):
-    new_seeds = []
+def map_seeds_to_new_values(map_values, last_seed_ranges):
+    new_seed_range = []
     
-    for seed in last_seeds:
-        new_seed = None
+    for seed_range in last_seed_ranges:
+        leftovers = None
         for map_value in map_values:
             [destination_start, source_start, map_range] = map_value
+            map_range = range(source_start, source_start + map_range)
 
-            if seed in range(source_start, source_start + map_range):
-                new_seed = destination_start + (seed - source_start)
+            # within range
+            #     [          ] 
+            #         #####
+            if (seed_range[0] >= map_range[0]) and (seed_range[-1] <= map_range[-1]):
+                shift_start = seed_range[0] - map_range[0]
+                shift_end = seed_range[-1] - map_range[0]
+                new_seed_range.append(range(destination_start + shift_start, destination_start + shift_end))
                 break
-        new_seeds.append(new_seed if new_seed else seed)
-    return new_seeds
+            
+            # outside range left
+            #     [          ] 
+            #  #########
+            if (seed_range[0] < map_range[0]) and (seed_range[-1] <= map_range[-1]):
+                leftovers = range(seed_range[0], map_range[0])
+                shift_start = 0
+                shift_end = seed_range[-1] - map_range[0]
+                new_seed_range.append(range(destination_start + shift_start, destination_start + shift_end))
+                continue
+                
+            # outside range right
+            #     [          ] 
+            #           ###########
+            if (seed_range[0] >= map_range[0]) and (seed_range[-1] > map_range[-1]):
+                leftovers = range(map_range[0], seed_range[0])
+                shift_start = seed_range[0] - map_range[0]
+                shift_end = map_range[-1]
+                new_seed_range.append(range(destination_start + shift_start, destination_start + shift_end))
+
+            
+            # print(map_range)
+        #     if seed in range(source_start, source_start + map_range):
+        #         new_seed = destination_start + (seed - source_start)
+        #         break
+        # new_seeds.append(new_seed if new_seed else seed)
+       
+    # new_seeds = []
+    
+    # for seed in last_seeds:
+    #     new_seed = None
+    #     for map_value in map_values:
+    #         [destination_start, source_start, map_range] = map_value
+
+    #         if seed in range(source_start, source_start + map_range):
+    #             new_seed = destination_start + (seed - source_start)
+    #             break
+    #     new_seeds.append(new_seed if new_seed else seed)
+    # return new_seeds
 
 def main():
     last_seed_numbers = []
-    with open("../input.txt") as f:
-    # with open("../input_example.txt") as f:
+    # with open("../input.txt") as f:
+    with open("../input_example.txt") as f:
         file_lines = f.readlines()
         last_seed_numbers = get_seeds(file_lines[0])
         map_values = []
@@ -49,82 +92,8 @@ def main():
             rest_file_len = (len(file_lines[2:]) - 1)
             if index == rest_file_len:
                 last_seed_numbers = map_seeds_to_new_values(map_values, last_seed_numbers)
-            print("progress: ", round(index * 100 / rest_file_len), "%")
+            # print("progress: ", round(index * 100 / rest_file_len), "%")
             
-    return min(last_seed_numbers)
+    # return min(last_seed_numbers)
 
 print(main())
-# import sys
-# import re
-# from collections import defaultdict
-# D = open(sys.argv[1]).read().strip()
-# L = D.split('\n')
-
-# parts = D.split('\n\n')
-# seed, *others = parts
-# seed = [int(x) for x in seed.split(':')[1].split()]
-
-# class Function:
-#   def __init__(self, S):
-#     lines = S.split('\n')[1:] # throw away name
-#     # dst src sz
-#     self.tuples: list[tuple[int,int,int]] = [[int(x) for x in line.split()] for line in lines]
-#     #print(self.tuples)
-#   def apply_one(self, x: int) -> int:
-#     for (dst, src, sz) in self.tuples:
-#       if src<=x<src+sz:
-#         return x+dst-src
-#     return x
-
-#   # list of [start, end) ranges
-#   def apply_range(self, R):
-#     A = []
-#     for (dest, src, sz) in self.tuples:
-#       src_end = src+sz
-#       NR = []
-#       while R:
-#         # [st                                     ed)
-#         #          [src       src_end]
-#         # [BEFORE ][INTER            ][AFTER        )
-#         (st,ed) = R.pop()
-#         # (src,sz) might cut (st,ed)
-#         before = (st,min(ed,src))
-#         inter = (max(st, src), min(src_end, ed))
-#         after = (max(src_end, st), ed)
-#         if before[1]>before[0]:
-#           NR.append(before)
-#         if inter[1]>inter[0]:
-#           A.append((inter[0]-src+dest, inter[1]-src+dest))
-#         if after[1]>after[0]:
-#           NR.append(after)
-#       R = NR
-#     return A+R
-
-# Fs = [Function(s) for s in others]
-
-# def f(R, o):
-#   A = []
-#   for line in o:
-#     dest,src,sz = [int(x) for x in line.split()]
-#     src_end = src+sz
-
-# P1 = []
-# for x in seed:
-#   for z in Fs:
-#     x = z.apply_one(x)
-#   P1.append(x)
-# print(min(P1))
-
-# P2 = []
-# pairs = list(zip(seed[::2], seed[1::2]))
-# for st, sz in pairs:
-#   # inclusive on the left, exclusive on the right
-#   # e.g. [1,3) = [1,2]
-#   # length of [a,b) = b-a
-#   # [a,b) + [b,c) = [a,c)
-#   R = [(st, st+sz)]
-#   for x in Fs:
-#     R = x.apply_range(R)
-#   #print(len(R))
-#   P2.append(min(R)[0])
-# print(min(P2))
