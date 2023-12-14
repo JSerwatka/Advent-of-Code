0a469a7fa7dc0a1dcda05436b1524146755d3326
Jakub Serwatka <33938646+JSerwatka@users.noreply.github.com>
2023-12-14 13:05:46 +0100

    add 9a, 9b 2023

---
+++ 2023/09/python/9b.py
@@ -0,0 +1,29 @@
+from typing import List
+
+
+def get_line_value(numbers: List[int]):
+    uniq_numbers = set(numbers)
+    if len(uniq_numbers) == 1 and list(uniq_numbers)[0] == 0:
+        return 0
+
+    next_line = []
+    for index in range(len(numbers)):
+        try:
+            difference = numbers[index + 1] - numbers[index]
+            next_line.append(difference)
+        except IndexError:
+            history = get_line_value(next_line)
+            history = numbers[0] - history
+            return history
+        
+def main():
+    with open("../input.txt") as f:
+    # with open("../input_example.txt") as f:
+        total = 0
+        for line in f:
+            numbers = [*map(int, line.strip().split(" "))]
+            history = get_line_value(numbers)
+            total += history
+    return total
+
+print(main())
